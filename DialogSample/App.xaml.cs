1851dcae7985522b86c075cb8c6dbcd87b25e109
锋 杨 <yangf85@163.com>
2025-01-04 18:23:13 +0800

    添加项目文件。

---
+++ DialogSample/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DialogSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+           
+        }
+    }
+
+}
---
+++ DialogSample/PersonViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogSample;
+
+public partial class PersonViewModel:ObservableObject
+{
+
+    [ObservableProperty]
+    string _name;
+    [ObservableProperty]
+    int _age;
+    [ObservableProperty]
+    string _address;
+    [ObservableProperty]
+    string _phone;
+    [ObservableProperty]
+    string _email;
+    [ObservableProperty]
+    string _website;
+
+
+}
